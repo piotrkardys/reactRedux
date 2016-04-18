d813d909c952d9f4a08d94c750256f5092b8ae14
piotrkardys <piotrk@mobilewebpro.pl>
2016-04-18 18:27:55 +0200

    Lesson 8: creating Counter block, operating on store's methods

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 "18.04.2016 - 14:00 - start of the project" 
 "18.04.2016 - 16:30 – Lesson 1 – 6: preparing the environment, react &babel configuration, creating files: index.html, main.js, webpack.config.js" 
 "18.04.2016 - 17:20 – Lesson 7: creating redux store from scratch, update file main.js" 
+"18.04.2016 - 18:20 – Lesson 8: creating Counter block, operating on store's methods, update files index.html and main.js" 
 
--- main.js
+++ main.js
@@ -1,4 +1,29 @@
-//import { createStore } from 'redux';
+import { createStore } from 'redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//const createStore = (reducer) => {
+//	let state;
+//	let listeners = [];								
+//
+//	const getState = () => state;									//returns actual state
+//
+//	const dispatch = (action) => {									//updates state and calls each listener in the array
+//		state = reducer(state, action);
+//		listeners.forEach(listener => listener());
+//	};
+//
+//	const subscribe = (listener) => {								//adds listeners to the array and filters them
+//		listeners.push(listener);
+//		return () => {
+//			listeners = listeners.filter(l => l !== listener);
+//		};
+//	};
+//
+//	dispatch({});													//returns function to the primary state
+//
+//	return { getState, dispatch, subscribe };
+//};
 
 const counter = (state = 0, action) => {			//function to increment/decrement the state
 	switch (action.type) {
@@ -8,47 +33,38 @@ const counter = (state = 0, action) => {			//function to increment/decrement the
 	}
 }
 
-const createStore = (reducer) => {
-	let state;
-	let listeners = [];								
+const store = createStore(counter);					//(redux's store)ours store
 
-	const getState = () => state;									//returns actual state
+const Counter = (props) => (						//main page (counter block)
+	<div>
+		<h1>{props.value}</h1>
+		<button onClick={props.onIncrement}>+</button>
+		<button onClick={props.onDecrement}>-</button>
+	</div>
 
-	const dispatch = (action) => {									//updates state and calls each listener in the array
-		state = reducer(state, action);
-		listeners.forEach(listener => listener());
-	};
+);
 
-	const subscribe = (listener) => {								//adds listeners to the array and filters them
-		listeners.push(listener);
-		return () => {
-			listeners = listeners.filter(l => l !== listener);
-		};
-	};
-
-	dispatch({});													//returns function to the primary state
-
-	return { getState, dispatch, subscribe };
+const render = () => {								//shows the block (render the Counter with proper parameters)
+//	document.body.innerText = store.getState();
+	ReactDOM.render(<Counter 
+		value={store.getState()}
+		onIncrement = {() => store.dispatch({type: 'INCREMENT'})}
+		onDecrement = {() => store.dispatch({type: 'DECREMENT'})} />, document.getElementById('root'));
 };
 
-const store = createStore(counter);					//(redux's store)ours store
-//const render = () => {
+store.subscribe(render);
+render();
+
+
+//console.log(store.getState());						//returns state
+
+//store.dispatch( { type: 'INCREMENT'});				//updates state (calls specific actions)
+//console.log(store.getState());
+
+//store.subscribe(() => {								//method is called every time when sth on the page change (after dispatch method)
 //	document.body.innerText = store.getState();
-//};
+//});
 
-//store.subscribe(render);
-//render();
-
-
-console.log(store.getState());						//returns state
-
-store.dispatch( { type: 'INCREMENT'});				//updates state (calls specific actions)
-console.log(store.getState());
-
-store.subscribe(() => {								//method is called every time when sth on the page change (after dispatch method)
-	document.body.innerText = store.getState();
-});
-
-document.addEventListener('click', () => {			//if there is click on the page do something (INCREMENT in that case)
-	store.dispatch({ type: 'INCREMENT'});
-});
+//document.addEventListener('click', () => {			//if there is click on the page do something (INCREMENT in that case)
+//	store.dispatch({ type: 'INCREMENT'});
+//});
