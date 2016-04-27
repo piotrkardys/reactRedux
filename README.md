918d42af5fdeb7b0c24d3c66fb0481815058d889
piotrkardys <piotrk@mobilewebpro.pl>
2016-04-27 12:57:44 +0200

    Lesson 28: changing AddTodo component (adding function connect)

--- README.md
+++ README.md
@@ -21,3 +21,4 @@
 "25.04.2016 – 15:50 – Lesson 24: changing the global variable store into parameter of the components, update file main.js" 
 "25.04.2016 – 17:00 – Lesson 25: creating class Provider which provides 'context' to the child components, update file main.js" 
 "25.04.2016 – 18:25 – Lesson 27: dividing VisibleTodoList into 3 functions linked with the connect function, update file main.js" 
+"27.04.2016 – 13:00 – Lesson 28: changing AddTodo component (adding function connect), update file main.js" 
--- main.js
+++ main.js
@@ -499,7 +499,7 @@ const TodoApp = () => (
 //render();
 */
 
-/* LESSON 24 - 27 */
+/* LESSON 24 - 27 */ /*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
@@ -719,6 +719,189 @@ const TodoApp = () => (
 //Provider.childContextTypes = {			//it is important to specify PropTypes - otherwise children don't get that prop
 //	store: React.PropTypes.object
 //};
+
+
+ReactDOM.render(<Provider store={createStore(todoApp)}>
+					<TodoApp />
+				</Provider>, 
+				document.getElementById('root'));
+*/
+
+/* LESSON 28 - */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider, connect } from 'react-redux';
+
+
+const todo = (state, action) => {								
+	switch (action.type) {
+		case 'ADD_TODO': return { id: action.id, text: action.text, completed: false };
+
+		case 'TOGGLE_TODO': 
+			if (state.id !== action.id) return state;		
+			return Object.assign({}, state, { completed: !state.completed });
+
+		default: return state;
+	};
+};
+
+const todos = (state = [], action) => {
+	switch (action.type) {
+		case 'ADD_TODO': return [...state, todo(undefined, action)];
+
+		case 'TOGGLE_TODO': return state.map(t => todo(t, action));
+
+		default: return state;
+	}
+};
+
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
+	switch(action.type) {
+		case 'SET_VISIBILITY_FILTER': return action.filter;
+		default: return state;
+	};
+};
+
+
+
+const todoApp = combineReducers({todos, visibilityFilter});
+
+
+
+const Link = ({active, children, onClick}) => {
+	if (active) {								
+		return <span>{children}</span>	
+	}
+	//
+	return (
+		<a href="#" onClick={e => {
+			e.preventDefault();
+			onClick();
+		}}>
+			{children}
+		</a>
+	);
+	
+};
+//
+
+class FilterLink extends React.Component {
+	componentDidMount() {
+		const {store} = this.context;
+		this.unsubscribe = store.subscribe(() => this.forceUpdate());
+	}
+	componentWillUnmount() {
+		this.unsubscribe();
+	}
+
+	render() {
+		const props = this.props;
+		const {store} = this.context;
+		const state = store.getState();
+
+		return (
+			<Link active={props.filter === state.visibilityFilter} onClick={() => 
+				store.dispatch({ type: 'SET_VISIBILITY_FILTER', filter: props.filter})}>
+				{props.children}
+			</Link>
+		);
+	}
+};
+//
+FilterLink.contextTypes = {
+	store: React.PropTypes.object
+};
+
+const Footer = () => {
+	return (
+		<div>
+			<p>Show: 
+				{' '}
+				<FilterLink filter='SHOW_ALL'>ALL, </FilterLink>
+				{' '}
+				<FilterLink filter='SHOW_ACTIVE'>ACTIVE, </FilterLink>
+				{' '}
+				<FilterLink filter='SHOW_COMPLETED'>COMPLETED</FilterLink>
+			</p>
+		</div>
+	);
+};
+//
+
+
+let nextTodoId = 0;
+let AddTodo = ({dispatch}) => { 		//we only need the dispatch method from the props object
+	let input;
+
+	return (
+		<div>
+			<input ref={node => { input = node}} />
+
+			<button onClick={() => {
+				dispatch({ type: 'ADD_TODO', text: input.value, id: nextTodoId++ });
+				input.value = '';
+			}}>
+				Add Todo
+			</button>
+		</div>
+	);
+};
+//
+//AddTodo = connect(state => { return {}; }, dispatch => { return { dispatch } };) (AddTodo); //we don't need to pass any props nor dispatchers (connect provides that from the context)
+//AddTodo = connect(null, dispatch => { return { dispatch } };) (AddTodo); 		    		  //so we could use that form
+AddTodo = connect() (AddTodo);																  //or even that most simplified form ({dispatch} will be given) - we can use that form if we only need the dispatch method (without any props)
+
+
+const TodoList = ({todos, onTodoClick}) => {
+	return (
+		<ul>
+			{todos.map(todo => <Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo.id)} />)}
+		</ul>
+	);
+};
+
+const Todo = ({onClick, completed, text}) => {
+	return (
+		<li onClick={onClick} style={{textDecoration: completed ? 'line-through' : 'none'}}> 
+				{text} 
+		</li>
+	);
+};
+//
+
+const getVisibleTodos = (todos, filter) => {
+	switch(filter) {
+		case 'SHOW_ALL': return todos;
+
+		case 'SHOW_ACTIVE': return todos.filter(t => !t.completed);
+
+		case 'SHOW_COMPLETED': return todos.filter(t => t.completed);
+	}
+};
+
+const mapStateToTodoListProps = (state) => {
+	return {
+		todos: getVisibleTodos(state.todos, state.visibilityFilter)	
+	};
+};
+const mapDispatchToTodoListProps = (dispatch) => {
+	return {
+		onTodoClick: (id) => { dispatch({ type: 'TOGGLE_TODO', id }) }
+	};
+};
+const VisibleTodoList = connect(mapStateToTodoListProps, mapDispatchToTodoListProps)(TodoList);
+
+
+const TodoApp = () => (
+
+	<div>
+		<AddTodo />
+		<VisibleTodoList />
+		<Footer />
+	</div>
+);
+//
 
 
 ReactDOM.render(<Provider store={createStore(todoApp)}>
